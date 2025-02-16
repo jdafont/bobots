e7bf61d2c5018c4a2bbacd6120631de356a1e8ad
Alex Fontenot <jdafontenot@gmail.com>
2025-02-16 16:52:05 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Bobots
+Just a playground for some robotic ideas.
+
+# Todo
+
+* Ability to represent a 'world'
+* Simulate in timesteps
+* Ability to represent bot state (actual as well as 'believed')
+* Ability to build a controller (holds beliefs, gets sensor inputs, produces motor outputs)
+* Ability to get sensor inputs
+* Ability to produce motor outputs
---
+++ app/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "bobots/core/vec2.h"
+
+int add(int a, int b) {
+	return a + b;
+}
+
+int main() {
+	bobot::Vec2_t v1(3, 4);
+	bobot::Vec2_t v2(1, 1);
+	bobot::Vec2_t v3 = v1;
+	v3.translate(3, 5);
+	v3.rotate(0.9);
+	std::cout << "v3 is " << v3.toString() << std::endl;
+
+	std::cout << "Norm is " << v1.norm() << std::endl;
+	v3.add(&v2);
+	std::cout << "Added norm is " << v3.norm() << std::endl;
+	std::cout << "Hello world" << std::endl;
+	std::cout << add(3, 4) << std::endl;
+	return 0;
+}
